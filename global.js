593cf85cccfd34d7c4eb4e14b1749c774b9ff358
terantaon <trtgong@gmail.com>
2025-10-17 22:02:36 -0700

    Update global.js

--- global.js
+++ global.js
@@ -37,6 +37,7 @@ for (let p of pages) {
 
 document.body.insertAdjacentHTML(
   'afterbegin',
+  `
 	<label class="color-scheme">
 		Theme:
 		<select>
@@ -44,7 +45,7 @@ document.body.insertAdjacentHTML(
       <option value='light'>Light</option>
       <option value='dark'>Dark</option>
 		</select>
-	</label>,
+	</label>`,
 );
 
 let select = document.querySelector('select');
