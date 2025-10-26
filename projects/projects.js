0caa460399c626cca676de94587b8ba323ab542e
terantaon <trtgong@gmail.com>
2025-10-25 22:25:09 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,19 +1,11 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-const BASE_PATH =
-  location.hostname === "localhost" || location.hostname === "127.0.0.1"
-    ? "../"
-    : "/portfolio/";
-
-const projects = await fetchJSON(`${BASE_PATH}lib/projects.json`);
+const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
-if (projects && projectsContainer) {
+renderProjects(projects, projectsContainer, 'h2');
 
-  renderProjects(projects, projectsContainer, 'h2');
-
-  const title = document.querySelector('.projects-title');
-  if (title) {
-    title.textContent = `${projects.length} ` + title.textContent;
-  }
+const title = document.querySelector('.projects-title');
+if (title) {
+  title.textContent = `${projects.length} ` + title.textContent;
 }
