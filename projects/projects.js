234d97177fda961670c6d8285ad3c6ffb4f865bf
terantaon <trtgong@gmail.com>
2025-10-25 21:58:39 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -3,4 +3,7 @@ import { fetchJSON, renderProjects } from '../global.js';
   const projects = await fetchJSON('../lib/projects.json');
   const projectsContainer = document.querySelector('.projects');
   renderProjects(projects, projectsContainer, 'h2');
+  if (title) {
+    title.textContent = `Projects`;
+  }
 })();
