4fd039676d711eac46ffe922b8a921a11f08ccf9
terantaon <trtgong@gmail.com>
2025-10-25 20:12:49 -0700

    fetch request

--- global.js
+++ global.js
@@ -60,3 +60,16 @@ select.addEventListener('input', function (event) {
   document.documentElement.style.setProperty('color-scheme', event.target.value);
   localStorage.colorScheme = event.target.value;
 });
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    console.log(response);
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
