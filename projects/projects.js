6b83b40b6dfb75e35a2c9ce5c08036d2dae982b2
terantaon <trtgong@gmail.com>
2025-11-01 00:30:39 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -15,48 +15,49 @@ let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-let rolledData = d3.rollups(
-  projects,
-  (v) => v.length,
-  (d) => d.year,
-);
+function renderPieChart(projectsGiven) {
+  // re-calculate rolled data
+  let newRolledData = d3.rollups(
+    projectsGiven,
+    (v) => v.length,
+    (d) => d.year,
+  );
+  // re-calculate data
+  let newData = newRolledData.map(([year, count]) => {
+    return { value: count, label: year }; // TODO
+  });
+  // re-calculate slice generator, arc data, arc, etc.
+  let newSliceGenerator = d3.pie().value((d) => d.value);
+  let newArcData = newSliceGenerator(newData);
+  let newArcs = newArcData.map((d) => arcGenerator(d));
 
-let data = rolledData.map(([year, count]) => {
-  return { value: count, label: year };
-});
+  let newSVG = d3.select('svg');
+  newSVG.selectAll('path').remove();
+  let newLegend = d3.select('.legend');
+  newLegend.selectAll('li').remove();
 
-let sliceGenerator = d3.pie().value((d) => d.value);
-let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
-
-arcs.forEach((arc, idx) => {
-    d3.select('svg')
+  newArcs.forEach((arc, idx) => {
+    newSVG
       .append('path')
       .attr('d', arc)
       .attr('fill', colors(idx))
-})
+    })
 
-let legend = d3.select('.legend');
-data.forEach((d, idx) => {
-  legend
-    .append('li')
-    .attr('style', `--color:${colors(idx)}`)
-    .attr('class', 'item')
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
-});
+    newData.forEach((d, idx) => {
+      newLegend
+      .append('li')
+      .attr('style', `--color:${colors(idx)}`)
+      .attr('class', 'item')
+      .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+    });
+}
 
-let query = '';
-
-let searchInput = document.querySelector('.searchBar');
+// Call this function on page load
+renderPieChart(projects);
 
 searchInput.addEventListener('change', (event) => {
-  // update query value
-  query = event.target.value;
-
-  let filteredProjects = projects.filter((project) => {
-    let values = Object.values(project).join('\n').toLowerCase();
-    return values.includes(query.toLowerCase());
-  });
-
+  let filteredProjects = setQuery(event.target.value);
+  // re-render legends and pie chart when event triggers
   renderProjects(filteredProjects, projectsContainer, 'h2');
+  renderPieChart(filteredProjects);
 });
