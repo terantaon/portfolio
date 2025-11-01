f4210816b75c528ceaa645d6a6191f296c4021fc
terantaon <trtgong@gmail.com>
2025-10-31 23:26:00 -0700

    update js

--- projects/projects.js
+++ projects/projects.js
@@ -39,6 +39,6 @@ let legend = d3.select('.legend');
 data.forEach((d, idx) => {
   legend
     .append('li')
-    .attr('style', `--color:${colors(idx)}`)
+    .attr('style', `--color:${colors(idx)}`) 
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
