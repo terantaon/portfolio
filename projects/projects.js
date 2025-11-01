ef386f0a02427fb4f858147688e3e0cacad81679
terantaon <trtgong@gmail.com>
2025-10-31 22:21:45 -0700

    Add a pie

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
@@ -9,3 +10,20 @@ const title = document.querySelector('.projects-title');
 if (title) {
   title.textContent = `${projects.length} ` + title.textContent;
 }
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+let data = [1, 2, 3, 4, 5, 5];
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc, idx) => {
+    d3.select('svg')
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', colors(idx))
+})
